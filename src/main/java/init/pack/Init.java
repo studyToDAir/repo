69736520b6ebe46d20a8bba163eb01e6f03b55d2
minsu <study.todair@gmail.com>
2022-09-01 17:07:13 +0900

    최초 등록

--- src/main/java/init/pack/Init.java
+++ src/main/java/init/pack/Init.java
@@ -1,4 +1,4 @@
-package human.pot.team;
+package init.pack;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Test
+ * Servlet implementation class Init
  */
-@WebServlet("/Test")
-public class Test extends HttpServlet {
+@WebServlet("/Init")
+public class Init extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Test() {
+    public Init() {
         super();
         // TODO Auto-generated constructor stub
     }
