527e1efa8947ac1563cc908eb8cce24397299064
minsu <study.todair@gmail.com>
2022-09-01 17:12:47 +0900

    url 패턴 수정

--- src/main/java/init/pack/Init.java
+++ src/main/java/init/pack/Init.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Init
  */
-@WebServlet("/Init")
+@WebServlet("/Init2")
 public class Init extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
